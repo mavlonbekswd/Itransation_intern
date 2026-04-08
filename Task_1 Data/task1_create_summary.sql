2ad62d24d2171cd7f926931e22ace225dfaa5b7b
Mavlonbek Sultanbekov <151122666+mavlonbekswd@users.noreply.github.com>
2026-04-08 17:47:10 +0100

    Refactor SQL script to create yearly summary table

--- Task_1 Data/task1_create_summary.sql
+++ Task_1 Data/task1_create_summary.sql
@@ -1,35 +1,40 @@
 use books_db
 
-Select TOP 5 *
-From dbo.yearly_summary
-order by average_price_usd desc;
+IF OBJECT_ID('dbo.yearly_summary', 'U') IS NOT NULL
+    DROP TABLE dbo.yearly_summary;
+GO
 
+SELECT
+    year AS publication_year,
+    COUNT(*) AS book_count,
+    CAST(
+        ROUND(
+            AVG(
+                CASE
+                    WHEN currency_symbol = '$' THEN price_numeric
+                    WHEN currency_symbol = N'€' THEN price_numeric * 1.2
+                END
+            ),
+            2
+        ) AS DECIMAL(10,2)
+    ) AS average_price_usd
+INTO dbo.yearly_summary
+FROM dbo.books_raw
+GROUP BY year;
+GO
 
-
-Select TOP 1
-    genre,
-    COUNT(*) AS total_books
-From dbo.books_raw
-Group BY genre
-Order BY total_books desc;
-
-Select TOP 5
-    author,
-    COUNT(*) AS books_written
-From dbo.books_raw
-Group BY author
-order BY books_written DESC;
-
-select
-    publication_year,
-    book_count,
-    book_count - LAG(book_count) over (ORDER BY publication_year) AS growth
-from dbo.yearly_summary
-ORDER BY publication_year;
-
-SELECT TOP 1
-    publication_year,
-    book_count,
-    book_count - LAG(book_count) OVER (ORDER BY publication_year) AS growth
+SELECT *
 FROM dbo.yearly_summary
-ORDER BY growth DESC;
+ORDER BY publication_year;
+GO
+
+SELECT COUNT(*) AS raw_row_count FROM dbo.books_raw;
+
+SELECT COUNT(*) AS summary_row_count FROM dbo.yearly_summary;
+
+SELECT COUNT(*) AS summary_row_count FROM dbo.yearly_summary;
+
+
+
+
+
